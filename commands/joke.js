0c2a82332c6debb63f68ab863eb477f8ec117542
quinacon <72992452+quinacon@users.noreply.github.com>
2024-06-19 21:28:23 +0200

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -1,23 +1,18 @@
 const httpRequest = require("../functions/httpsRequest.js");
 
 function getJoke(callback) {
-    // Define the options for the HTTP request
     const options = {
         hostname: 'v2.jokeapi.dev',
         path: '/joke/Any?safe-mode&blacklistFlags=nsfw,religious,political,racist,sexist,explicit',
         method: 'GET',
     };
 
-    // Make the HTTP request using the httpRequest module
     httpRequest(options)
         .then(response => {
-            // Parse the JSON response
             const jokeData = JSON.parse(response.data);
-            // Pass the joke data to the callback function
             callback(null, jokeData);
         })
         .catch(error => {
-            // If there is an error with the HTTP request, pass it to the callback
             callback(error, null);
         });
 }
