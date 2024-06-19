596aad8a450e8fcff67ca22d9b9baa3f5657ac61
quinacon <72992452+quinacon@users.noreply.github.com>
2024-06-19 21:23:53 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -3,24 +3,17 @@ const config = require("./config.js")
 const fs = require('fs');
 const path = require('path');
 
-// Define the directory where your command files are located
 const commandsDir = path.join(__dirname, 'commands');
 
-// Initialize an empty array to store the commands
 let commands = [];
 
-// Read all files in the commands directory
 fs.readdirSync(commandsDir).forEach(file => {
-    // Check if the file is a JavaScript file
     if (file.endsWith('.js')) {
-        // Require the command file
         const command = require(path.join(commandsDir, file));
-        // Push the command object into the commands array
         commands[command.name] = command
     }
 });
 
-//adds dynamic commands command
 commands["commands"] = {
     function: function (message) {
         let formattedString = `**Prefix**: ${config.prefix}\n\n**Commands:**\n`;
@@ -35,5 +28,4 @@ commands["commands"] = {
     description: "Lists up all commands."
 }
 
-// Export the array of commands
 module.exports = commands;
